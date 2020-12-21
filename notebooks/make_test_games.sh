3ea89b3d4445aa2ee09762375f015fcf5681556d
Marcos Mota <marcosfmmota@gmail.com>
2020-12-21 16:47:36 -0300

    Create make_test_games.sh

---
+++ notebooks/make_test_games.sh
@@ -0,0 +1,5 @@
+tw-make custom --quest-max-length 15 --seed 1234 --output games/custom_game1.ulx
+tw-make custom --quest-max-length 15 --seed 2134 --output games/custom_game2.ulx
+tw-make custom --quest-max-length 15 --seed 3124 --output games/custom_game3.ulx
+tw-make custom --quest-max-length 15 --seed 3214 --output games/custom_game4.ulx
+tw-make custom --quest-max-length 15 --seed 4321 --output games/custom_game5.ulx
